6f809397e550d950b5adf565035187fe4d462426
vmcguire@unitedlocating.com+1-at-530555730387 <vmcguire@unitedlocating.com>
2022-06-22 15:21:57 -0600

    added cancel button, console log user inputs

--- create-ticket.js
+++ create-ticket.js
@@ -1,218 +1,234 @@
 function ShowCreateTicket() {
-  if (typeof (_CreateTicketPopup) !== "undefined") {
-    _CreateTicketPopup.show();
-    return;
-  }
-const formView = $('<div/>');
+  // if (typeof (_CreateTicketPopup) !== "undefined") {
+  //   _CreateTicketPopup.show();
+  //   return;
+  // }
+
+  const formView   = $('<div />');
+
   //No html, the following is appended to the body of the ui
 
   _CreateTicketPopup = $('<div>').appendTo('body')
-   .dxPopup({
-      title: 'Create Ticket',
-      visible: true,
-      width: '100%',
-      height: 900,
-      
-    
-      contentTemplate: (container) => {
+  .dxPopup({
+    title: 'Create Ticket',
+    visible: true,
+    //use precent baseed width to support multiple window sizes
+    width: '100%',
+    height: '100%',
+    contentTemplate: (container) => {
 
-        //Create jquery elements
-        const scrollView = $('<div />');
-  
-        //'turn' form view into dev extreme form
-        formView.dxForm({ 
-          formData: {},
-          items: [
-            {
-              "dataField": "ADDITIONALMEMBERS"
-            },
-            {
-              "dataField": "ADDRESS"
-            },
-            {
-              "dataField": "ALTPHONE"
-            },
-            {
-              "dataField": "CALLER"
-            },
-            {
-              "dataField": "CALLERPHONE"
-            },
-            {
-              "dataField": "CITY"
-            },
-            {
-              "dataField": "COMPANY"
-            },
-            {
-              "dataField": "COMPANYADDRESS"
-            },
-            {
-              "dataField": "CONTACT"
-            },
-            {
-              "dataField": "CONTACTFAX"
-            },
-            {
-              "dataField": "CONTACTPHONE"
-            },
-            {
-              "dataField": "COUNTY"
-            },
-            {
-              "dataField": "DURATION"
-            },
-            {
-              "dataField": "EMAILADDRESS"
-            },
-            {
-              "dataField": "EMAIL"
-            },
-            {
-            "dataField": "EXPLOSIVES"
-            },
-            {
-              "dataField": "XLOC"
-            },
-            {
-              "dataField": "YLOC"
-            },
-            {
-              "dataField": "LOCATIONOFWORK"
-            },
-            {
-              "dataField": "MAPREF"
-            },
-            {
-              "dataField": "NEARESTINTERSECTINGSTREET"
-            },
-            {
-              "dataField": "CreatedBy"
-            },
-            {
-              "dataField": "PRIORITY"
-            },
-            {
-              "dataField": "ROW"
-            },
-            {
-              "dataField": "SENDTO"
-            },
-            {
-              "dataField": "SEQUENCENUMBER"
-            },
-            {
-              "dataField": "STATE"
-            },
-            {
-              "dataField": "STREET"
-            },
-            {
-              "dataField": "OC_TICKETNUMBER"
-            },
-            {
-              "dataField": "TRANSMITDATE"
-            },
-            {
-              "dataField": "TRANSMITOP"
-            },
-            {
-              "dataField": "TRANSMITTIME"
-            },
-            {
-              "dataField": "TUNNELBORE"
-            },
-            {
-              "dataField": "TYPEOFWORK"
-            },
-            {
-              "dataField": "UPDATEOF"
-            },
-            {
-              "dataField": "WORKBEINGDONEFOR"
-            },
-            {
-              "dataField": "DUEDATE"
-            },
-            {
-              "dataField": "DUETIME"
-            },
-            {
-              "dataField": "RECEIVEDDATE"
-            },
-            {
-              "dataField": "RECEIVEDTIME"
-            },
-            {
-              "dataField": "REMARKS"
-            },
-            {
-              "dataField": "TWP"
-            },
-            {
-              "dataField": "RNG"
-            },
-            {
-              "dataField": "ASR_PARSER"
-            },
-            {
-              "dataField": "PARSER_NAME"
-            },
-            {
-              "dataField": "OCC_TICKET_IDENTIFIER"
-            },
-            {
-              "dataField": "OCC_TICKET_VERSION"
-            },
-            {
-              "dataField": "ZIP"
-            }
-          ]
-        })
-  
-        //append dev extreme form to jquery scroll view
-        scrollView.append(formView);
-  
-        //'turn' scroll view into dev extreme scrollview
-        scrollView.dxScrollView({
-          width: '100%',
-          height: '100%',
-        })
-  
-        //append dev extreme scrollView to outer container
-        container.append(scrollView);
-  
-      },
-     
+      //Create jquery elements
+      const scrollView = $('<div />');
+
+      //'turn' form view into dev extreme form
+      formView.dxForm({ 
+        formData: {},
+        items: [
+          {
+            dataField: "ADDITIONALMEMBERS",
+            validationRules: [{
+              type: 'required',
+              message: 'This field is required',
+            }], 
+          },
+          {
+            "dataField": "ADDRESS",
+            validationRules: [{
+              type: 'required',
+              message: 'Address is required',
+            }], 
+          },
+          {
+            "dataField": "ALTPHONE"
+          },
+          {
+            "dataField": "CALLER"
+          },
+          {
+            "dataField": "CALLERPHONE"
+          },
+          {
+            "dataField": "CITY"
+          },
+          {
+            "dataField": "COMPANY"
+          },
+          {
+            "dataField": "COMPANYADDRESS"
+          },
+          {
+            "dataField": "CONTACT"
+          },
+          {
+            "dataField": "CONTACTFAX"
+          },
+          {
+            "dataField": "CONTACTPHONE"
+          },
+          {
+            "dataField": "COUNTY"
+          },
+          {
+            "dataField": "DURATION"
+          },
+          {
+            "dataField": "EMAILADDRESS"
+          },
+          {
+            "dataField": "EMAIL"
+          },
+          {
+          "dataField": "EXPLOSIVES"
+          },
+          {
+            "dataField": "XLOC"
+          },
+          {
+            "dataField": "YLOC"
+          },
+          {
+            "dataField": "LOCATIONOFWORK"
+          },
+          {
+            "dataField": "MAPREF"
+          },
+          {
+            "dataField": "NEARESTINTERSECTINGSTREET"
+          },
+          {
+            "dataField": "CreatedBy"
+          },
+          {
+            "dataField": "PRIORITY"
+          },
+          {
+            "dataField": "ROW"
+          },
+          {
+            "dataField": "SENDTO"
+          },
+          {
+            "dataField": "SEQUENCENUMBER"
+          },
+          {
+            "dataField": "STATE"
+          },
+          {
+            "dataField": "STREET"
+          },
+          {
+            "dataField": "OC_TICKETNUMBER"
+          },
+          {
+            "dataField": "TRANSMITDATE"
+          },
+          {
+            "dataField": "TRANSMITOP"
+          },
+          {
+            "dataField": "TRANSMITTIME"
+          },
+          {
+            "dataField": "TUNNELBORE"
+          },
+          {
+            "dataField": "TYPEOFWORK"
+          },
+          {
+            "dataField": "UPDATEOF"
+          },
+          {
+            "dataField": "WORKBEINGDONEFOR"
+          },
+          {
+            "dataField": "DUEDATE"
+          },
+          {
+            "dataField": "DUETIME"
+          },
+          {
+            "dataField": "RECEIVEDDATE"
+          },
+          {
+            "dataField": "RECEIVEDTIME"
+          },
+          {
+            "dataField": "REMARKS"
+          },
+          {
+            "dataField": "TWP"
+          },
+          {
+            "dataField": "RNG"
+          },
+          {
+            "dataField": "ASR_PARSER"
+          },
+          {
+            "dataField": "PARSER_NAME"
+          },
+          {
+            "dataField": "OCC_TICKET_IDENTIFIER"
+          },
+          {
+            "dataField": "OCC_TICKET_VERSION"
+          },
+          {
+            "dataField": "ZIP"
+          }
+        ]
+      })
+
+      //append dev extreme form to jquery scroll view
+      scrollView.append(formView);
+
+      //'turn' scroll view into dev extreme scrollview
+      scrollView.dxScrollView({
+        width: '100%',
+        height: '100%',
+      })
+
+      //append dev extreme scrollView to outter container
+      container.append(scrollView);
+    },
     toolbarItems: [ {
       widget: "dxButton",
       toolbar:'bottom',
       location: "after",
       options: { 
-          
           text: "Submit", 
           icon:'key',
           type: "success",
-          onClick:(e)=>{ 
-            attemptSubmit()
-            .then(()=>{
-              document.location.href = 'index.html';
-            })
-            .catch((err)=>{
-              console.log(err);
-              DevExpress.ui.notify('Invalid attempt. Please try again.','error',3000);
-            })
+          onClick:(e)=>{
+            //print form data
+
+            if(formView.dxForm('instance').validate()){
+              console.log(formView.dxForm('instance').option('formData'));
+
+            }
+
           }
-        } , 
-   }],
- 
+        },
+    },
+    {
+      widget: "dxButton",
+      toolbar:'bottom',
+      location: "after",
+      options: { 
+          text: "Cancel", 
+          icon:'clear',
+          type: "danger",
+          onClick:(e)=>{
+            console.log("Submission cancelled")
+          }
+      },
+    }
+  ]
   })
-  
-  .dxPopup('instance')
-  }
-  
-  
-  $(() => {
-    ShowCreateTicket();
-  })
+}
+
+$(() => {
+  ShowCreateTicket();
+})
+
   
